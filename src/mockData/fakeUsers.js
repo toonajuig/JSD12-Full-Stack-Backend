f7a73f79ee25e48e2c63ed63cf4db49488daeb42
toonajuig <thirawat.devtt@gmail.com>
2026-05-13 15:15:46 +0700

    created backend

---
+++ src/mockData/fakeUsers.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: 1,
+    username: "Alice",
+    email: "alice.johnson@example.com",
+    password: "password123",
+  },
+  {
+    id: 2,
+    username: "Bob",
+    email: "bob.smith@example.com",
+    password: "password456",
+  },
+  {
+    id: 3,
+    username: "Charlie",
+    email: "charlie.brown@example.com",
+    password: "password789",
+  },
+];
